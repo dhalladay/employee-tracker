2d840819a786c12e15e620e092b17b555b19449f
dhalladay <dave.halladay@outlook.com>
2022-01-23 16:12:30 -0700

    update schema datatypes

--- db/schema.sql
+++ db/schema.sql
@@ -4,14 +4,14 @@ DROP TABLE IF EXISTS department;
 
 CREATE TABLE department (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(50) NOT NULL,
+  name VARCHAR(30) NOT NULL,
   CONSTRAINT uc_name UNIQUE (name)
 );
 
 CREATE TABLE role (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(50) NOT NULL,
-  salary FLOAT NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
   department_id INTEGER,
   CONSTRAINT fk_dept FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
