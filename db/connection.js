e7ab78f120a2cbc1523f49c36f9efacb2aed3fe6
daveh <dave.halladay@outlook.com>
2022-01-30 19:42:51 -0700

    added host to connection.js

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const db = mysql.createConnection(
   {
-    host: process.env.DB_HOST,
+    host: 'localhost',
     user: process.env.DB_USER,
     password: process.env.DB_KEY,
     database: process.env.DB_DATABASE
